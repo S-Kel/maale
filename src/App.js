410b656fe0439082f79c4e93e9312d714c8e4025
Stephen Kel <s.kel@student.qut.edu.au>
2019-01-07 16:11:58 +1100

    added navbar and materialize css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
+import Header from "./components/headers/header";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">Nath Maale!</header>
+      <div className="container">
+        <Header />
       </div>
     );
   }
---
+++ src/components/headers/header.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function header() {
+  return (
+    <nav>
+      <div className="nav-wrapper blue-grey darken-4">
+        <a href="google.com" className="brand-logo center">
+          <i class="material-icons">cloud</i>
+          YeYo!
+        </a>
+        <ul id="nav-mobile" class=" left hide-on-med-and-down">
+          <li>
+            <a href="sass.html">Home</a>
+          </li>
+          <li>
+            <a href="badges.html">About Us</a>
+          </li>
+          <li>
+            <a href="collapsible.html">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default header;
